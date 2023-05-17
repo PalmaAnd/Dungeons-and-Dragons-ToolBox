839b6fb4f56111de426329cc313d531bf978f5f0
PalmaAnd <andrepalma@outlook.de>
2023-05-17 22:38:24 +0200

    Started making changes for the generator page

--- src/pages/generator.tsx
+++ src/pages/generator.tsx
@@ -1,15 +1,12 @@
 // pages/generator.tsx
 
 import { useState } from "react";
-import Link from "next/link";
 
 const GeneratorPage = () => {
   const [diceResult, setDiceResult] = useState<number | null>(null);
 
-  const handleGenerateClick = () => {
-    const diceNumber = Math.floor(Math.random() * 6) + 1;
-    const diceType = Math.floor(Math.random() * 3) + 1;
-    const result = diceNumber * diceType;
+  const handleGenerateClick = (dice:number) => {
+    const result = Math.floor(Math.random() * dice) + 1;
     setDiceResult(result);
   };
 
@@ -19,14 +16,14 @@ const GeneratorPage = () => {
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <div
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            onClick={handleGenerateClick}
+            onClick={() => handleGenerateClick(4)}
           >
             <h3 className="text-2xl font-bold">D4</h3>
             <div className="text-lg">{diceResult && <p className="result">Result: {diceResult}</p>}</div>
           </div>
           <div
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            onClick={handleGenerateClick}
+            onClick={() => handleGenerateClick(6)}
           >
             <h3 className="text-2xl font-bold">D6</h3>
             <div className="text-lg">{diceResult && <p className="result">Result: {diceResult}</p>}</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,9 +23,7 @@ const Home: NextPage = () => {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/generator"
-              target="_blank"
-            >
+              href="/generator">
               <h3 className="text-2xl font-bold">Random things Generator →</h3>
               <div className="text-lg">
                 Just the basics - Everything you need to run a game.
