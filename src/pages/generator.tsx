b041cb371a80d9007778629a73b2331253c0d94f
PalmaAnd <andrepalma@outlook.de>
2023-05-17 23:02:10 +0200

    Changed the way the blocks will be shown and how the random number will be generated

--- src/pages/generator.tsx
+++ src/pages/generator.tsx
@@ -3,31 +3,38 @@
 import { useState } from "react";
 
 const GeneratorPage = () => {
-  const [diceResult, setDiceResult] = useState<number | null>(null);
+  const [D4, setD4] = useState<number | null>(null);
+  const [D6, setD6] = useState<number | null>(null);
+  const [D8, setD8] = useState<number | null>(null);
 
-  const handleGenerateClick = (dice:number) => {
-    const result = Math.floor(Math.random() * dice) + 1;
-    setDiceResult(result);
+  const generateD4 = () => {
+    const result = Math.floor(Math.random() * 4) + 1;
+    setD4(result);
+  };
+
+  const generateD6 = () => {
+    const result = Math.floor(Math.random() * 6) + 1;
+    setD6(result);
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2c2d35] to-[#3f0101]">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <div
+          <button
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            onClick={() => handleGenerateClick(4)}
+            onClick={generateD4}
           >
             <h3 className="text-2xl font-bold">D4</h3>
-            <div className="text-lg">{diceResult && <p className="result">Result: {diceResult}</p>}</div>
-          </div>
-          <div
+            <div className="text-lg">Result: {D4 && <p className="result">{D4}</p>}</div>
+          </button>
+          <button
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            onClick={() => handleGenerateClick(6)}
+            onClick={generateD6}
           >
             <h3 className="text-2xl font-bold">D6</h3>
-            <div className="text-lg">{diceResult && <p className="result">Result: {diceResult}</p>}</div>
-          </div>
+            <div className="text-lg">Result: {D6 && <p className="result">{D6}</p>}</div>
+          </button>
         </div>
       </div>
     </main>
