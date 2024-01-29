818cc5a49e8071e3d06a8d263bde3b9bb7a097e6
André Palma <54452893+PalmaAnd@users.noreply.github.com>
2024-01-29 14:50:01 +0100

    Removed Header problem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import Header from "./components/Header";
 
 const Home: NextPage = () => {
   return (
