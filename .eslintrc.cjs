f2e47e9bd74b0d2a0c2be6f96df74820dc32d624
André Palma <54452893+PalmaAnd@users.noreply.github.com>
2024-03-07 18:50:30 +0100

    Updated whole project to newer t3-stack version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,37 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require("path");
-
 /** @type {import("eslint").Linter.Config} */
 const config = {
-	overrides: [
-		{
-			extends: [
-				"plugin:@typescript-eslint/recommended-requiring-type-checking",
-			],
-			files: ["*.ts", "*.tsx"],
-			parserOptions: {
-				project: path.join(__dirname, "tsconfig.json"),
-			},
-		},
-	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		project: path.join(__dirname, "tsconfig.json"),
-	},
-	plugins: ["@typescript-eslint"],
-	extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-	rules: {
-		"@typescript-eslint/consistent-type-imports": [
-			"warn",
-			{
-				prefer: "type-imports",
-				fixStyle: "inline-type-imports",
-			},
-		],
-		"@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-	},
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
+  rules: {
+    // These opinionated rules are enabled in stylistic-type-checked above.
+    // Feel free to reconfigure them to your own preference.
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
+  },
 };
 
 module.exports = config;
---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  tabWidth: 2,
+	useTabs: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	trailingComma: "all",
+	jsxBracketSameLine: false,
+	endOfLine: "auto",
+	semi: true,
+	printWidth: 80,
+	arrowParens: "always",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
